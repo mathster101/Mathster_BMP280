1783fdbf4077192d140b6c9ff6f68dcd9b88d15d
Mathew Simon Panicker <mpanicker@altiostar.com>
2021-09-09 22:36:30 +0530

    additions

--- Mathster_BMP280.cpp
+++ Mathster_BMP280.cpp
@@ -2,3 +2,37 @@
 #include "Wire.h"
 
 
+uint8_t BMP280_Mathew::i2c_read_byte(const uint8_t addr) {
+  Wire.beginTransmission(device_address);
+  Wire.write(addr);
+  Wire.endTransmission();
+  Wire.requestFrom(device_address, 1);
+  while (Wire.available() != 1)
+    ; // wait till device is actually available
+  return (uint8_t)Wire.read();
+}
+
+uint8_t* BMP280_Mathew::i2c_read_bytes(const uint8_t addr, uint8_t* buffer_to_fill, int num_bytes) {
+  Wire.beginTransmission(device_address);
+  Wire.write(addr);
+  Wire.endTransmission();
+  Wire.requestFrom(device_address, num_bytes);
+  while (Wire.available() != num_bytes)
+      ; // wait till device is actually available
+  for (int i = 0; i < num_bytes; i++) {
+    buffer_to_fill[i] = Wire.read();
+  }
+  return buffer_to_fill;
+}
+
+uint8_t BMP280_Mathew::i2c_write_byte(const uint8_t addr, const uint8_t data_byte) {
+  Wire.beginTransmission(device_address);
+  Wire.write(addr);
+  Wire.write(data_byte);
+  Wire.endTransmission();
+  if (i2c_read_byte(addr) == data_byte)
+    return true;
+  else
+    return false;
+}
+
--- Mathster_BMP280.h
+++ Mathster_BMP280.h
@@ -2,7 +2,13 @@
 
 
 class BMP280_Mathster {
+public:
+  void initialize();
+  double get_pressure();
+  float get_temperature();
+
 private:
+  uint8_t device_address = 0x76;
   uint8_t ctrl_meas = 0xF4;
   uint8_t config = 0xF5;
   uint8_t temp_reg_start = 0xFA;                                                           //till and including 0xFC
@@ -10,13 +16,9 @@ private:
   uint16_t dig_T1, dig_P1;                                                                 //calibration
   uint8_t calibration_reg_start = 0x88;                                                    //till and including 0x9F ,i.e. 24 bytes
   int32_t dig_T2, dig_T3, dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;  //calibration
-  int32_t t_fine;                                                                          //internal use only
-public:
-  void initialize();
-  uint8_t device_address = 0x76;
+  int32_t t_fine = -100000;                                                                          //internal use only
+  
   uint8_t i2c_read_byte(const uint8_t addr);
   uint8_t* i2c_read_bytes(const uint8_t addr, uint8_t* buffer_to_fill, int num_bytes);
   uint8_t i2c_write_byte(const uint8_t addr, const uint8_t data_byte);
-  double get_pressure();
-  float get_temperature();
 };
