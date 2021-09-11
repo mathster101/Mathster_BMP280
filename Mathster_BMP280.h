1e2838474034e93010dcdc00bec31d9513ab517a
Mathew Simon Panicker <mpanicker@altiostar.com>
2021-09-11 22:23:25 +0530

    added comments

--- Mathster_BMP280.h
+++ Mathster_BMP280.h
@@ -1,3 +1,12 @@
+/*
+* Library for the BMP280 Pressure and Temperature Sensor
+* Uses I2C
+* Developed by Mathew P.
+* Datasheet can be found at [shorturl.at/alzDG] (Bosch website)
+* Thanks to Bosch SensorTec for calculation details [https://github.com/BoschSensortec/BMP280_driver]
+*/
+
+
 #include "Arduino.h"
 #include "Wire.h"
 
